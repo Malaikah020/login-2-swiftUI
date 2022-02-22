eec609c5243388c6e530ed6ba81697dafc2ecc7b
Malaikah Hafeez <Malaikah Hafeez>
2022-02-22 09:10:55 +0000

    Initial Commit

---
+++ notyet/notyetApp.swift
@@ -0,0 +1,17 @@
+//
+//  notyetApp.swift
+//  notyet
+//
+//  Created by Malaikah Hafeez on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct notyetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
